01f33216c610bf277851ce0742df7ca9dc22b951
Marina Fadel Bearzi <marina.bearzi@gmail.com>
2017-11-28 23:53:57 -0200

    Add files via upload

--- rou.js
+++ rou.js
@@ -3,13 +3,24 @@
 Functions to open and close menu
 -----------------------------------------------------------------------------
 */
-function w3_open() {
+function menu_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
-function w3_close() {
+function menu_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
+}
+function menu_list(item) {
+    var x = document.getElementById(item);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
 }
 /* Set Global Variables */
 var i = 0; 
@@ -376,7 +387,6 @@ function addVehicleType(input) {
                         if (document.getElementById("vtype_id_" + m)) {
                             var inputId = document.getElementById("vtype_id_" + m)
                             if(m == 0){
-                            	console.log("input1", input1)
                                 input1.options.length = 0
                                 var title = new Option();
                                 title.text = "";
--- sumocfg.js
+++ sumocfg.js
@@ -3,13 +3,24 @@
 Functions to open and close menu
 -----------------------------------------------------------------------------
 */
-function w3_open() {
+function menu_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
-function w3_close() {
+function menu_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
+}
+function menu_list(item) {
+    var x = document.getElementById(item);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        x.previousElementSibling.className.replace(" w3-green", "");
+    }
 }
 /*
 -----------------------------------------------------------------------------
